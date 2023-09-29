0f037465e50c4f4c67eb405d74ef0d217d706456
enbt0909 <eugenbeirit22@gmail.com>
2023-09-29 09:09:00 +0200

    button

---
+++ app.js
@@ -0,0 +1,26 @@
+const form = document.getElementById("multiplication-form");
+      const table = document.getElementById("multiplication-table");
+
+      form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const input = document.getElementById("number-input").value;
+        const number = Number(input);
+
+        if (!isNaN(number) && number <= 1000) {
+          table.innerHTML = "";
+
+          for (let i = 1; i <= 1000; i++) {
+            const row = document.createElement("tr");
+            const cell1 = document.createElement("td");
+            const cell2 = document.createElement("td");
+
+            cell1.textContent = `${number} * ${i}`;
+            cell2.textContent = `${number * i}`;
+
+            row.appendChild(cell1);
+            row.appendChild(cell2);
+            table.appendChild(row);
+          }
+        }
+      });
